319aa172dcc19f08e56f2dc0182c8ae3f9d94408
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-11 10:10:35 +0100

    stuff i think

--- src/hokaku.cpp
+++ src/hokaku.cpp
@@ -3,9 +3,35 @@
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("hokaku", "en-US")
 
+static void* output_create(obs_data_t* settings, obs_source_t* context)
+{
+}
+
+static void output_destroy(void* data)
+{
+}
+
+static void output_defaults(obs_data_t *settings)
+{
+}
+
+obs_source_info hokaku_output = (hokaku_output = obs_source_info(),
+    hokaku_output.id = "hokaku",
+    hokaku_output.type = OBS_SOURCE_TYPE_INPUT,
+    hokaku_output.output_flags = OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_DO_NOT_DUPLICATE,
+    hokaku_output.get_name = "Hokaku",
+    hokaku_output.create = output_create,
+    hokaku_output.destroy = output_destroy,
+    hokaku_output.get_defaults = output_defaults,
+    hokaku_output
+);
+
 bool obs_module_load(void)
 {
-  //TODO: Initialize named pipe and source
+    obs_register_source(&hokaku_output);
+
+    return true;
+  //TODO: Initialize named pipe
 }
 
 void obs_module_unload(void)
