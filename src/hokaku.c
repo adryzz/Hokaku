c325871d6ccc4fb228881573bca5c7b17c3220f5
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-14 18:37:57 +0100

    make shit actually work

--- src/hokaku.c
+++ src/hokaku.c
@@ -1,17 +1,9 @@
-#include <obs-module.h>
-//#include <boost/interprocess/shared_memory_object.hpp>
-//#include <boost/interprocess/mapped_region.hpp>
+#include </home/adryzz/Hokaku/external/obs-studio/libobs/obs-module.c>
 
 #define MODULE_ID "hokaku"
 #define MODULE_NAME "Hokaku"
-#define SHMEM_NAME "hokaku-input"
 
 OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE("hokaku", "en-US")
-
-//using boost::interprocess;
-
-//shared_memory_object shmem;
 
 static void* output_create(obs_data_t* settings, obs_source_t* context)
 {
@@ -30,21 +22,20 @@ struct obs_source_info hokaku_output =
     .type = OBS_SOURCE_TYPE_INPUT,
     .output_flags = OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_DO_NOT_DUPLICATE,
     .get_name = MODULE_NAME,
-    .icon_type = OBS_ICON_TYPE_CAMERA,
     .create = output_create,
     .destroy = output_destroy,
-);
+    .icon_type = OBS_ICON_TYPE_CAMERA,
+};
 
 bool obs_module_load(void)
 {
+    blog(LOG_INFO, "plugin loaded successfully");
     obs_register_source(&hokaku_output);
-    
-    //shmem = (create_only, SHMEM_NAME, read_write);
-    
+    blog(LOG_INFO, "source registered successfully");
     return true;
 }
 
 void obs_module_unload(void)
 {
-    //shared_memory_object::remove(SHMEM_NAME);
+    blog(LOG_INFO, "plugin unloaded");
 }
